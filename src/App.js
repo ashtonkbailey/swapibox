fed58ffe8ca38fd41a3d7be6af54d404ef78cd38
Ashton Bailey <ashtonkbailey@gmail.com>
2018-12-03 16:54:23 -0700

    Write basic components and prelim funcs in App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      favorites: [],
+      currFilm: {
+        title: '',
+        crawl: '',
+        year: ''
+      }
+    }
+  }
+
+
+
+  cleanFilm = (film) => {
+    //clean up opening crawl and year
+    //setState()
+  }
+
+  componentDidMount = () => {
+    //generateRandomNumber
+    //fetch film(randomNum)
+    //cleanFilm(selectedFilm)
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>SwapiBox</h1>
         </header>
+        <Navigation />
+        <Favorites />
+        <ContentContainer 
+          film={this.state.currFilm}
+        //<FilmText />
+        //<ContentCards 
+        //    <Card />
+        // />
+        />
       </div>
     );
   }
