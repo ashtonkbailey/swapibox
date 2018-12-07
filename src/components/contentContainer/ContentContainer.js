9b688ebff440eb8a53216819cb0e91b4734c9c3d
Ashton Bailey <ashtonkbailey@gmail.com>
2018-12-06 21:15:26 -0700

    First round of lint and write structures for rest of app tests

--- src/components/contentContainer/ContentContainer.js
+++ src/components/contentContainer/ContentContainer.js
@@ -4,50 +4,36 @@ import PropTypes from 'prop-types';
 import './ContentContainer.css';
 import PeopleCard from '../PeopleCard/PeopleCard';
 
-const ContentContainer = ({
-  carouselIndex,
-  incrementCarousel,
-  decrementCarousel,
-  film,
-  contents,
-}) => {
+const ContentContainer = ({carouselIndex, incrementCarousel, decrementCarousel, film, contents, addToFavorites}) => {
   let displayedContents;
   let containerClassName;
-  let card2;
-  let card3;
 
-const ContentContainer = ({carouselIndex, incrementCarousel, decrementCarousel, film, contents, addToFavorites}) => {
-    let displayedContents;
-    let containerClassName;
-   
+  if (!contents.length) {
+    displayedContents = (
+      <div>    
+      <div className="crawl">
+      <div>
+      {film.crawl}
+      </div>
+      </div>
+      <h3 className="title"> {film.title} {film.year} </h3>
+      </div>
+      );
+    containerClassName = "content-container";
+  }
+  else {
+    const peopleCards = contents.map((currCard) => {
+      return (<PeopleCard card={currCard}
+        carouselIndex={carouselIndex} addToFavorites={addToFavorites}/> )
+    });
 
-    if (!contents.length) {
-        displayedContents = (
-            <div>    
-                <div className="crawl">
-                    <div>
-                    {film.crawl}
-                    </div>
-                </div>
-                <h3 className="title"> {film.title} {film.year} </h3>
-            </div>
-        );
-        containerClassName = "content-container";
-    }
-    else {
-        const peopleCards = contents.map((currCard) => {
-            return (<PeopleCard card={currCard}
-            carouselIndex={carouselIndex} addToFavorites={addToFavorites}/> )
-        });
-
-        displayedContents = (
-            <div className="people-container">
-                {peopleCards}
-            </div>
-        );
-        containerClassName = "component-container";
-    }
-
+    displayedContents = (
+      <div className="people-container">
+      {peopleCards}
+      </div>
+      );
+    containerClassName = "component-container";
+  }
 
 ContentContainer.propTypes = {
   carouselIndex: PropTypes.number.isRequired,
