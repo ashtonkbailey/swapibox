cb9a217e70759cd3fc70121881fd3f1125eb206f
Ashton Bailey <ashtonkbailey@gmail.com>
2018-12-07 16:22:21 -0700

    Lint contentContainer

--- src/components/contentContainer/ContentContainer.js
+++ src/components/contentContainer/ContentContainer.js
@@ -5,62 +5,89 @@ import './ContentContainer.css';
 import PeopleCard from '../PeopleCard/PeopleCard';
 import PlanetCard from '../PlanetCard/PlanetCard';
 
-const ContentContainer = ({carouselIndex, incrementCarousel, decrementCarousel, film, contents, addToFavorites, chosenContent}) => {
-    let displayedContents;
-    let containerClassName;
-    if (!contents.length) {
-        displayedContents = (
-            <div>    
-                <div className="crawl">
-                    <div>
-                    {film.crawl}
-                    </div>
-                </div>
-                <h3 className="title"> {film.title} {film.year} </h3>
-            </div>
-        );
-        containerClassName = "content-container";
-    }
-    else { 
-      let contentCards;
-      // if(chosenContent === 'people') {
-         contentCards = contents.map((currCard) => {
-              return (<PeopleCard card={currCard}
-            carouselIndex={carouselIndex} addToFavorites={addToFavorites} key={currCard.name} chosenContent={chosenContent} /> )
-        });
-      // } 
+const ContentContainer = ({
+  carouselIndex,
+  incrementCarousel,
+  decrementCarousel,
+  film,
+  contents,
+  addToFavorites,
+  chosenContent,
+}) => {
+  let displayedContents;
+  let containerClassName;
 
-      // else if (chosenContent === 'planets') {
-      //   contentCards = contents.map((currCard) => {
-      //     return (<PlanetCard card={currCard}
-      //       carouselIndex={carouselIndex} addToFavorites={addToFavorites} key={currCard.name}/>)
-      //   });
-      // }
+  if (!contents.length) {
+    displayedContents = (
+      <div>
+        <div className="crawl">
+          <div>
+            {film.crawl}
+          </div>
+        </div>
+        <h3 className="title">
+          {film.title}
+          {film.year}
+        </h3>
+      </div>
+    );
+    containerClassName = 'content-container';
+  } else {
+    const contentCards = contents.map(currCard => (
+      <PeopleCard
+        card={currCard}
+        carouselIndex={carouselIndex}
+        addToFavorites={addToFavorites}
+        key={currCard.name}
+        chosenContent={chosenContent}
+      />
+    ));
+    // if(chosenContent === 'people') {
+    // }
 
-        displayedContents = (
-            <div className="people-container">
-                {contentCards}
-            </div>
-        );
-        containerClassName = "component-container";
-    }
+    // else if (chosenContent === 'planets') {
+    //   contentCards = contents.map((currCard) => {
+    //     return (<PlanetCard card={currCard}
+    //       carouselIndex={carouselIndex} addToFavorites={addToFavorites} key={currCard.name}/>)
+    //   });
+    // }
+    displayedContents = (
+      <div className="people-container">
+        {contentCards}
+      </div>
+    );
+    containerClassName = 'component-container';
+  }
 
-    return (
-        <div className={containerClassName}>
-            <button className={contents.length ? "arrow-button" : "none"} onClick={decrementCarousel}> <i className="fas fa-arrow-left"></i> </button>
-            {displayedContents}
-            <button className={contents.length ? "arrow-button" : "none"} onClick={incrementCarousel}> <i className="fas fa-arrow-right"></i> </button>
-         </div>
-    )
-}
+  return (
+    <div className={containerClassName}>
+      <button
+        type="button"
+        className={contents.length ? 'arrow-button' : 'none'}
+        onClick={decrementCarousel}
+      >
+        <i className="fas fa-arrow-left" />
+      </button>
+      {displayedContents}
+      <button
+        type="button"
+        className={contents.length ? 'arrow-button' : 'none'}
+        onClick={incrementCarousel}
+      >
+        <i className="fas fa-arrow-right" />
+      </button>
+    </div>
+  );
+};
 
 ContentContainer.propTypes = {
-    carouselIndex: PropTypes.number.isRequired,
-    incrementCarousel: PropTypes.func,
-    decrementCarousel: PropTypes.func,
-    film: PropTypes.object.isRequired,
-    contents: PropTypes.arrayOf(PropTypes.object),
-    addToFavorites: PropTypes.func
-}
+  carouselIndex: PropTypes.number.isRequired,
+  incrementCarousel: PropTypes.func.isRequired,
+  decrementCarousel: PropTypes.func.isRequired,
+  film: PropTypes.object.isRequired,
+  contents: PropTypes.arrayOf(PropTypes.object).isRequired,
+  addToFavorites: PropTypes.func.isRequired,
+  chosenContent: PropTypes.array.isRequired,
+};
 
 export default ContentContainer;
