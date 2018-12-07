decfa0a8be3cb91d652000cbf5edbfc57f1d85fb
Ashton Bailey <ashtonkbailey@gmail.com>
2018-12-07 11:45:19 -0700

    Lint planetCard

--- src/components/PlanetCard/PlanetCard.js
+++ src/components/PlanetCard/PlanetCard.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
 import lightsabers from '../../images/lightsabers.png';
 // import './planetCard.css';
 import luke from '../../images/luke.jpg';
 
-const PlanetCard = () => {
-  return (
-    <div> 
-      
-    </div>
-  )
-}
+const PlanetCard = () => (
+  <div>
 
+  </div>
+);
 
+PlanetCard.propTypes = {
 
-
+};
 
 export default PlanetCard;
