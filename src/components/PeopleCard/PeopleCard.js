d038e6b9a6606af30db9c72d44b14e716a9b00d3
Ashton Bailey <ashtonkbailey@gmail.com>
2018-12-07 12:20:15 -0700

    Lint peopleCard

--- src/components/PeopleCard/PeopleCard.js
+++ src/components/PeopleCard/PeopleCard.js
@@ -1,53 +1,113 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
 import lightsabers from '../../images/lightsabers.png';
 import './peopleCard.css';
-import luke from '../../images/luke.jpg'
+import luke from '../../images/luke.jpg';
 
-const PeopleCard = ({card, carouselIndex, addToFavorites, chosenContent}) => {
-    let hiddenClass = 'hidden';
-    let leftCardIndex = carouselIndex;
-    let middleCardIndex = carouselIndex + 1;
-    let rightCardIndex = carouselIndex + 2;
-    let allCards;
-    if (carouselIndex === 9) {
-      middleCardIndex = 0;
-      rightCardIndex = carouselIndex - 8;
-    }
-    if (carouselIndex === 8) {
-      rightCardIndex = carouselIndex - 8;
-    }
-    switch(card.index) {
-      case middleCardIndex:
-        hiddenClass = 'middle-card';
-        break;
-      case leftCardIndex:
-        hiddenClass = 'left-card small-card' ;
-        break;
-      case rightCardIndex:
-        hiddenClass = 'right-card small-card';
-        break;
-    }
-    if (chosenContent === 'people') {
-       return (<article className={hiddenClass}>
-        <h3 className="name"> {card.name} </h3>
-        <img src={luke} />
-        <p> species: {card.species} </p>
-        <p> homeworld: {card.homeworld} </p>
-        <p> population: {card.population} </p>
-        <img className="saber" src={lightsabers} onClick={() => addToFavorites({card})}/> 
-      </article>)
-    } else if (chosenContent === 'planets') {
-      return ( <article className={hiddenClass}>
-        <h3 className="name"> {card.name} </h3>
-        <img src={luke} />
-        <p> terrain: {card.terrain} </p>
-        <p> climate: {card.climate} </p>
-        <p> population: {card.population} </p>
-        <img className="saber" src={lightsabers} onClick={() => addToFavorites({card})}/>
+const PeopleCard = ({
+  card,
+  carouselIndex,
+  addToFavorites,
+  chosenContent,
+}) => {
+  let hiddenClass;
+  const leftCardIndex = carouselIndex;
+  let middleCardIndex = carouselIndex + 1;
+  let rightCardIndex = carouselIndex + 2;
+
+  if (carouselIndex === 9) {
+    middleCardIndex = 0;
+    rightCardIndex = carouselIndex - 8;
+  }
+
+  if (carouselIndex === 8) {
+    rightCardIndex = carouselIndex - 8;
+  }
+
+  switch (card.index) {
+    case middleCardIndex:
+      hiddenClass = 'middle-card';
+      break;
+    case leftCardIndex:
+      hiddenClass = 'left-card small-card';
+      break;
+    case rightCardIndex:
+      hiddenClass = 'right-card small-card';
+      break;
+    default:
+      hiddenClass = 'hidden';
+  }
+
+  if (chosenContent === 'people') {
+    return (
+      <article className={hiddenClass}>
+        <h3 className="name">
+          {card.name}
+        </h3>
+        <img
+          src={luke}
+          alt="character"
+        />
+        <p>
+          species:
+          {card.species}
+        </p>
+        <p>
+          homeworld:
+          {card.homeworld}
+        </p>
+        <p>
+          population:
+          {card.population}
+        </p>
+        <img
+          className="saber"
+          src={lightsabers}
+          onClick={() => addToFavorites({ card })}
+          alt="click to add to favorites"
+        />
       </article>
-      )
-    }
-}
+    );
+  }
+  if (chosenContent === 'planets') {
+    return (
+      <article className={hiddenClass}>
+        <h3 className="name">
+          {card.name}
+        </h3>
+        <img
+          src={luke}
+          alt="planet"
+        />
+        <p>
+          terrain:
+          {card.terrain}
+        </p>
+        <p>
+          climate:
+          {card.climate}
+        </p>
+        <p>
+          population:
+          {card.population}
+        </p>
+        <img
+          className="saber"
+          src={lightsabers}
+          onClick={() => addToFavorites({ card })}
+          alt="click to add to favorites"
+        />
+      </article>
+    );
+  }
+};
+
+PeopleCard.propTypes = {
+  card: PropTypes.object.isRequired,
+  carouselIndex: PropTypes.func,
+  addToFavorites: PropTypes.func,
+  chosenContent: PropTypes.array.isRequired,
+};
 
 export default PeopleCard;
