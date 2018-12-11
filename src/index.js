807a80080d1afcbffe4ff3fb8275ac5f99499b26
Cole Vanacore <colevanacore@gmail.com>
2018-12-10 17:41:47 -0700

    Install router

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from 'react-router-dom'
+
+const router = (
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+)
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
