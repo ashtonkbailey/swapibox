32e947c918e071b6bb93cca88e27039dc47b52cd
Cole Vanacore <colevanacore@gmail.com>
2018-12-11 16:09:32 -0700

    Add favorite cards to router

--- src/components/favorites/Favorites.js
+++ src/components/favorites/Favorites.js
@@ -7,28 +7,42 @@ import { Route, NavLink } from 'react-router-dom';
 
 
 const Favorites = ({ favorites }) => {
+  let favoritesMessage;
+  let stat1;
+  let stat2;
+  let stat3;
+  if(favorites.length === 0) {
+    favoritesMessage = "You do not have any favorites!";
+  }
 
   const allFavorites = favorites.map(fave => {
+      if (fave.species) {
+        stat1 = `species: ${fave.species}`;
+        stat2 = `homeworld: ${fave.homeworld}`;
+        stat2 = `population: ${fave.population}`;
+      } else if (fave.terrain) {
+        stat1 = `terrain: ${fave.terrain}`;
+        stat2 = `climate: ${fave.climate}`;
+        stat3 = `population: ${fave.population}`
+      } else {
+        stat1 = `model: ${fave.model}`;
+        stat2 = `class: ${fave.class}`;
+        stat3 = `passengers: ${fave.passengers}`
+      }
+
     return (
       <article className="fave-card" key={fave.name}>
         <h3 className="name">
           {fave.name}
         </h3>
-        {/* <img
-          src={luke}
-          alt="character"
-        /> */}
         <p>
-          species:
-          {fave.species || fave.terrain}
+          {stat1}
         </p>
         <p>
-          homeworld:
-          {/* {fave.homeworld} */}
+          {stat2}
         </p>
         <p>
-          population:
-          {fave.population}
+          {stat3}
         </p>
         <img
           className="saber"
@@ -45,6 +59,7 @@ const Favorites = ({ favorites }) => {
           X
         </NavLink>
       <h1> FAVORITES </h1>
+      <span className="favorites-message">  {favoritesMessage} </span>
       {allFavorites}
     </div>
   )
