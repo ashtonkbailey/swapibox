cd81a293bfef82ec6a29e3c045047597bee48838
Ashton Bailey <ashtonkbailey@gmail.com>
2018-12-11 21:01:09 -0700

    Lint data file

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom';
 
 const router = (
   <BrowserRouter>
     <App />
   </BrowserRouter>
-)
+);
 
 ReactDOM.render(router, document.getElementById('root'));
 
