de086ec99641f8493a02bdb53bc8a9207e2518aa
Ashton Bailey <ashtonkbailey@gmail.com>
2018-12-11 21:23:16 -0700

    Lint Navigation

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,41 +1,40 @@
 import React from 'react';
-import './Navigation.css';
 import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
+
+import './Navigation.css';
 import lightsabers from '../../images/lightsabers.png';
 
-const Navigation = ({favorites}) => (
-  <div className="nav-container" >
-    <NavLink to='/people' className="nav-button people" >
+const Navigation = ({ favorites }) => (
+  <div className="nav-container">
+    <NavLink to="/people" className="nav-button people">
     people
     </NavLink>
-    <NavLink to='/planets' className="nav-button planets" >
+    <NavLink to="/planets" className="nav-button planets">
     planets
     </NavLink>
-    <NavLink to='/vehicles' className="nav-button vehicle" >
+    <NavLink to="/vehicles" className="nav-button vehicle">
     vehicle
     </NavLink>
-    <NavLink to="/favorites" className="favorites-container" >
-          <div
-              className="num-favorites"
-              // onClick={viewFavorites}
-            >
-              {favorites.length}
-            </div>
-            <img
-              className="lightsaber"
-              src={lightsabers}
-              alt="favorites"
-            />
-              view favorites
+    <NavLink to="/favorites" className="favorites-container">
+      <div
+        className="num-favorites"
+          // onClick={viewFavorites}
+      >
+        {favorites.length}
+      </div>
+      <img
+        className="lightsaber"
+        src={lightsabers}
+        alt="favorites"
+      />
+      view favorites
     </NavLink>
-            
   </div>
 );
 
-
 Navigation.propTypes = {
-  displayChosenContent: PropTypes.func.isRequired,
+  favorites: PropTypes.array.isRequired,
 };
 
 export default Navigation;
