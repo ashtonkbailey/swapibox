a2a6c3f9bf4a2a69a15ca3482eea3c972818397c
Ashton Bailey <ashtonkbailey@gmail.com>
2018-12-11 21:53:50 -0700

    Lint Favorites

--- src/components/favorites/Favorites.js
+++ src/components/favorites/Favorites.js
@@ -1,34 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import '../contentContainer/ContentContainer.css'
+import { NavLink } from 'react-router-dom';
+
+import '../contentContainer/ContentContainer.css';
 import './Favorites.css';
 import lightsabers from '../../images/lightsabers.png';
-import { Route, NavLink } from 'react-router-dom';
-
 
 const Favorites = ({ favorites }) => {
   let favoritesMessage;
   let stat1;
   let stat2;
   let stat3;
-  if(favorites.length === 0) {
-    favoritesMessage = "You do not have any favorites!";
+
+  if (favorites.length === 0) {
+    favoritesMessage = 'You do not have any favorites!';
   }
 
-  const allFavorites = favorites.map(fave => {
-      if (fave.species) {
-        stat1 = `species: ${fave.species}`;
-        stat2 = `homeworld: ${fave.homeworld}`;
-        stat2 = `population: ${fave.population}`;
-      } else if (fave.terrain) {
-        stat1 = `terrain: ${fave.terrain}`;
-        stat2 = `climate: ${fave.climate}`;
-        stat3 = `population: ${fave.population}`
-      } else {
-        stat1 = `model: ${fave.model}`;
-        stat2 = `class: ${fave.class}`;
-        stat3 = `passengers: ${fave.passengers}`
-      }
+  const allFavorites = favorites.map((fave) => {
+    if (fave.species) {
+      stat1 = `species: ${fave.species}`;
+      stat2 = `homeworld: ${fave.homeworld}`;
+      stat2 = `population: ${fave.population}`;
+    } else if (fave.terrain) {
+      stat1 = `terrain: ${fave.terrain}`;
+      stat2 = `climate: ${fave.climate}`;
+      stat3 = `population: ${fave.population}`;
+    } else {
+      stat1 = `model: ${fave.model}`;
+      stat2 = `class: ${fave.class}`;
+      stat3 = `passengers: ${fave.passengers}`;
+    }
 
     return (
       <article className="fave-card" key={fave.name}>
@@ -51,23 +52,24 @@ const Favorites = ({ favorites }) => {
           alt="click to add to favorites"
         />
       </article>
-    )
+    );
   });
   return (
     <div className="faves-display">
-       <NavLink to='/people' className="nav-button hide-faves" >
-          X
-        </NavLink>
+      <NavLink to="/people" className="nav-button hide-faves">
+        X
+      </NavLink>
       <h1 className="favorite-header"> FAVORITES </h1>
-      <span className="favorites-message">  {favoritesMessage} </span>
+      <span className="favorites-message">
+        {favoritesMessage}
+      </span>
       {allFavorites}
     </div>
-  )
+  );
 };
 
 Favorites.propTypes = {
-  faves: PropTypes.array.isRequired,
-  viewFavorites: PropTypes.func.isRequired,
+  favorites: PropTypes.array.isRequired,
 };
 
 export default Favorites;
